414af93a8b6dde4db396eac2ad97fc965905b797
paul <paul@pauls-MacBook-Pro.local>
2018-10-20 01:12:40 -0700

    require story complete

--- TinderApp/CardsViewController.swift
+++ TinderApp/CardsViewController.swift
@@ -13,32 +13,58 @@ class CardsViewController: UIViewController {
     @IBOutlet weak var photoImage: UIImageView!
     
     var cardInitialCenter: CGPoint!
+    var divisor: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        divisor = (view.frame.width / 2) / 0.61
+        cardInitialCenter = photoImage.center
     }
     
     @IBAction func panGestureOnPhotoImage(_ sender: UIPanGestureRecognizer) {
         
         
-        view.transform = CGAffineTransform(rotationAngle: CGFloat(45 * M_PI / 180))
-        
-        let location = sender.location(in: view)
-        let velocity = sender.velocity(in: view)
+        let imageView = sender.view as! UIImageView
         let translation = sender.translation(in: view)
+        let location = sender.location(in: view)
+        let xFromCenter = imageView.center.x - view.center.x
+        
+        imageView.center = CGPoint(x: cardInitialCenter.x + translation.x, y: cardInitialCenter.y)
+        
+        if location.y < cardInitialCenter.y {
+            imageView.transform = CGAffineTransform(rotationAngle: xFromCenter / divisor)
+        } else {
+            imageView.transform = CGAffineTransform(rotationAngle: xFromCenter / -divisor)
+        }
         
         if sender.state == .began {
-            print("Gesture began")
+            print("Started pan")
         } else if sender.state == .changed {
-            print("Gesture is changing")
             
         } else if sender.state == .ended {
-            print("Gesture ended")
+            if imageView.center.x < cardInitialCenter.x - 50 {
+                //animate to the right
+                UIView.animate(withDuration: 0.3, animations: {
+                    imageView.center = CGPoint(x: imageView.center.x - 500, y: imageView.center.y)
+                    imageView.alpha = 0
+                })
+            } else if imageView.center.x > cardInitialCenter.x + 50 {
+                UIView.animate(withDuration: 0.3, animations: {
+                    imageView.center = CGPoint(x: imageView.center.x + 500, y: imageView.center.y)
+                    imageView.alpha = 0
+                })
+            } else {
+                imageView.center = cardInitialCenter
+                imageView.transform = CGAffineTransform.identity
+            }
         }
     }
     
     
-
+    @IBAction func doubleTapOnPhotoImage(_ sender: UITapGestureRecognizer) {
+        performSegue(withIdentifier: "ProfileModal", sender: nil)
+    }
+    
 }
---
+++ TinderApp/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  TinderApp
+//
+//  Created by paul on 10/20/18.
+//  Copyright © 2018 PoHung Wang. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
